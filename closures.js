8529c737ddc84d84a8364fa81e7835d3696fb370
Madison Fierro <madisonmfierro@gmail.com>
2018-05-24 18:53:53 -0700

    Closures and Constructors

--- closures.js
+++ closures.js
@@ -204,13 +204,10 @@ function secretNumber() {
 */
 
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
-    function save () {
-      return i;
-      setTimeout(function() {
-        console.log(i);
-      }, i * 1000);
-    }
+  for (let i = 0; i <= 5; i++) {
+    setTimeout(function() {
+      console.log(i);
+    }, i * 1000);
   }
 }
 timeOutCounter();
